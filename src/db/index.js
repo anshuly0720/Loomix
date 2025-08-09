050f9ba1c4f70519c6ad6591ca041c5451d95554
anshuly0720 <aannsshhuull0007@gmail.com>
2025-08-09 01:29:28 +0000

    build some utilily function and handled Api error and response

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,33 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+// const connectDB = async () => {
+//     try {
+//         const connectionInstance = await mongoose.connect(
+//             `${process.env.MONGODB_URI}/${DB_NAME}`
+//         );
+//         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+//     } catch (error) {
+//         console.log("MONGODB connection FAILED: ", error);
+//         process.exit(1)
+//     }
+// };
+
+// export default connectDB
+
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-    try {
+    try{
         const connectionInstance = await mongoose.connect(
             `${process.env.MONGODB_URI}/${DB_NAME}`
         );
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB connection FAILED: ", error);
-        process.exit(1)
+        console.log(`\n MONGODB connected !! DB HOST ${connectionInstance.connection.host}`);
+    } catch (error){
+        console.log("MONGODB connection failed: ",error);
+        process.exit(1);
     }
-};
+}
 
 export default connectDB
